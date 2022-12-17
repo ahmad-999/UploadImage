512ff23ef50bab499884456e3c9ce2220f18e75b
ahmad-999 <kuteshahmad@gmail.com>
2022-12-17 15:33:30 +0200

    17-12-2022

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{any}',function (){return view('spa');})->where('any','.*');
+Route::get('/{any}',function (){return view('spa');})->where('any','(.*)?');
 // Route::get('/{any}',fn () => view('spa'))->where('any','.*');
